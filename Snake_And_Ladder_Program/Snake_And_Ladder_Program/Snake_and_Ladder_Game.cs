be6b87abfbcdf84295fa64ed46578961a3db26b4
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-06-23 21:44:54 +0530

    [Niketan]Add:Usecase 4 for Player to reach till the Winning Position 100

--- Snake_And_Ladder_Program/Snake_And_Ladder_Program/Snake_and_Ladder_Game.cs
+++ Snake_And_Ladder_Program/Snake_And_Ladder_Program/Snake_and_Ladder_Game.cs
@@ -10,46 +10,66 @@ namespace Snake_And_Ladder_Program
     {
         public void Game()
         {
-            Console.WriteLine("Please Press Enter to Start the Game");
-            Console.ReadLine();
-
             int playerPosition = 0;
-            Random random = new Random();
+            int diceRoll;
+            bool gameCompleted = false;
 
-            int diceRoll = random.Next(1, 7);
-            if(diceRoll > 0)
+            while (!gameCompleted)
             {
+                Console.WriteLine("Please Press Enter to Start the Game");
+                Console.ReadLine();
+                Random random = new Random();
+                diceRoll = random.Next(1, 7);
+
                 Console.WriteLine("The Dice Rolls is : " + diceRoll);
                 playerPosition += diceRoll;
-            }
-            else
-            {
-                Console.WriteLine("There is NoPlay, Please Roll the Dice Again");
+
+                int ladderPosition;
+
+                if (playerPosition == 100)
+                {
+                    Console.WriteLine("The Player's Position is : " + playerPosition);
+                    Console.WriteLine("Haaleluya.......Player Wins The Game");
+                }
+                else if (playerPosition > 100)
+                {
+                    Console.WriteLine("You have Exceeded the Maximum Position, You need to Restart the Game");
+                    playerPosition = 0;
+                }
+                else if (playerPosition < 0)
+                {
+                    Console.WriteLine("The Player is at Position Zero");
+                    playerPosition = 0;
+                }
+                else
+                {
+                    Console.WriteLine("Player Position is: " + playerPosition);
+                }
+
+                switch (playerPosition)
+                {
+                    case 1:
+                        ladderPosition = 15;
+                        Console.WriteLine("OOPS,The player has been Eaten by a Giant Snake");
+                        playerPosition -= ladderPosition;
+                        Console.WriteLine("Player Position is : " + playerPosition);
+                        break;
+
+                    case 2:
+                        ladderPosition = 65;
+                        Console.WriteLine("YAY, The Player just reached to a ladder");
+                        playerPosition += ladderPosition;
+                        Console.WriteLine("Player Position is : " + playerPosition);
+                        break;
+                    case 3:
+                        Console.WriteLine("There will be NoPlay in the Game, Player Stays at Same Position");
+                        Console.WriteLine("Player Position is : " + playerPosition);
+                        break;
+
+                }
             }
 
-            
-            int ladderPosition;
 
-            int option = random.Next(1,3);
-
-            switch(option)
-            {
-                case 1: 
-                    Console.WriteLine("There will be NoPlay in the Game, Player Stays at Same Position");
-                    
-                   break;
-                case 2:
-                    ladderPosition = 65;
-                    Console.WriteLine("YAY, The Player just reached to a ladder");
-                    playerPosition += ladderPosition;
-                    break;
-                case 3:
-                    ladderPosition = 15;
-                    Console.WriteLine("OOPS,The player has been Eaten by a Giant Snake");
-                    playerPosition -= ladderPosition;
-                    break;
-            }
-            Console.WriteLine("The Player's Position is : " + playerPosition);
         }
     }
 }
