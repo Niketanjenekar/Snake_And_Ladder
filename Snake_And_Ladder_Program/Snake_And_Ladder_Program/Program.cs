4689cf6b665bfbf1abe9192f004cb80939fe695f
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-06-23 16:49:49 +0530

    [Niketan]Add:Welcome to Snake and Ladder Program

---
+++ Snake_And_Ladder_Program/Snake_And_Ladder_Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_And_Ladder_Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //**************************Day-4 Assignment***********************
+
+            Console.WriteLine("Welcome to Snake And Ladder Program");
+
+            Console.ReadLine();
+        }
+    }
+}
